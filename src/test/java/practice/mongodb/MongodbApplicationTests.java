77251c8e1f4943e08bd47a32009baf3227904027
yun <vivalavida.devv@gmail.com>
2023-12-22 06:48:09 +0900

    test: 등록된 빈 확인하기

--- src/test/java/practice/mongodb/MongodbApplicationTests.java
+++ src/test/java/practice/mongodb/MongodbApplicationTests.java
@@ -1,13 +1,22 @@
 package practice.mongodb;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootTest
 class MongodbApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
+		String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+		for (String beanDefinitionName : beanDefinitionNames) {
+			System.out.println(beanDefinitionName);
+		}
 	}
-
 }
