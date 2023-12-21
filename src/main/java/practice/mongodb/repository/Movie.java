b07d9663c4550175b205f596239c156847a1473f
yun <vivalavida.devv@gmail.com>
2023-12-22 08:20:11 +0900

    feat: spring data repository 용  Movie Entity 추가

---
+++ src/main/java/practice/mongodb/repository/Movie.java
@@ -0,0 +1,38 @@
+package practice.mongodb.repository;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+public class Movie {
+    private String id;
+    private String title;
+    private List<Director> directors;
+    private List<Genre> genres;
+
+    public Movie(String title, List<Director> directors, List<Genre> genres) {
+        this.title = title;
+        this.directors = directors;
+        this.genres = genres;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @ToString
+    public static class Director {
+        String name;
+    }
+
+
+    @Getter
+    public enum Genre {
+        A, B, C;
+    }
+}
